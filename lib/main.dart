d220996ba98cd07338f79a4058cff79162799d29
Akash Angadi <81625153+AKASHANGADII@users.noreply.github.com>
2022-08-08 23:14:40 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title:"Xylophone app",
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
